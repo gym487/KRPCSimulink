a7ca86181bdcf05c098fd0df4583d0d1e93742d6
QingBiJuShi <gym487@hotmail.com>
2019-11-05 01:16:56 +0800

    rocket!

--- KRPC_Wrapper_SetData.m
+++ KRPC_Wrapper_SetData.m
@@ -1,7 +1,7 @@
 function KRPC_Wrapper_SetData(motor_power,throttle)
 global KRPC;
 KRPC{'interfControl'}.throttle = throttle;
-for i=1:4
-    KRPC{'interfEngines'}{i}.thrust_limit = motor_power(i);
-end
+%for i=1:4
+  %  KRPC{'interfEngines'}{1}.thrust_limit = motor_power(1);
+%end
 end
